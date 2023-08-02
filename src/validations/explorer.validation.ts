5422b4f32e5251f166e991e4a6f623efb237b8e1
Abdul Ahad <9919+AbdulAhadArain@users.noreply.github.com>
2023-08-02 15:15:24 +0500

    search api validation updated

--- src/validations/explorer.validation.ts
+++ src/validations/explorer.validation.ts
@@ -3,9 +3,9 @@ import Joi from 'joi';
 export const searchData = {
   query: Joi.object().keys({
     data: Joi.string().required(),
-    fromNetwork: Joi.string(),
-    toNetwork: Joi.string(),
-    fromDate: Joi.number(),
-    toDate: Joi.number(),
+    fromNetwork: Joi.string().allow(''),
+    toNetwork: Joi.string().allow(''),
+    fromDate: Joi.number().allow(''),
+    toDate: Joi.number().allow(''),
   }),
 };
